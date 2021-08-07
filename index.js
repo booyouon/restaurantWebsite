ecb38c58faf15527e58b060b28e4552ee915f5bc
booyouon <66927111+booyouon@users.noreply.github.com>
2021-08-07 15:20:07 -0700

    ejsmate, partials, layouts
    - ejsmate added as an npm package for the use of the layout function
    - layout used to create boilerplate.ejs which is then included into the index.ejs
    - navbar and header are turned into partials then included into index.ejs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const { default: fetch } = require('node-fetch');
 const mongoose = require('mongoose');
 const { response } = require('express');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/thekikibar', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
@@ -15,10 +16,12 @@ mongoose.connect('mongodb://localhost:27017/thekikibar', {useNewUrlParser: true,
         console.log(err);
     });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'))
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free'));
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
