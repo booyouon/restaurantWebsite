b5692be8635e24340e878f4cc6e4e641e73487e4
booyouon <66927111+booyouon@users.noreply.github.com>
2021-08-08 13:46:34 -0700

    CRUD in Review
    - method-override added and also urlencoded set to true in order to use post
    - Able to now create, update, and delete posts in the review section.

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const mongoose = require('mongoose');
 const { response } = require('express');
 const ejsMate = require('ejs-mate');
 const reviews = require('./models/reviews');
+const methodOverride = require('method-override');
 
-mongoose.connect('mongodb://localhost:27017/thekikibar', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/thekikibar', {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true})
     .then(() => {
         console.log("mongo connection open");
     })
@@ -21,6 +22,8 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free'));
 
@@ -33,6 +36,25 @@ app.get('/', async (req, res) => {
     const data = await punkapi.json();
     const review = await reviews.find({});
     res.render('index', { data, review });
+})
+
+app.post('/', async (req,res) => {
+    const newReview = await new reviews(req.body);
+    await newReview.save();
+    console.log(newReview);
+    res.redirect('/');
+})
+
+app.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const review = await reviews.findByIdAndUpdate(id, req.body);
+    res.redirect('/')
+})
+
+app.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    await reviews.findByIdAndDelete(id);
+    res.redirect('/')
 })
 
 app.use((req,res) => {
